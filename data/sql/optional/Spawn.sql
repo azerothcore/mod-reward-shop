9545b0372d108726c52837ad6728afbf6e7f8158
St0ny <42348051+Raz0r1337@users.noreply.github.com>
2026-02-07 15:26:19 +0100

    Add files via upload
    Add extra NPCs that exchange the original ones with coderedeem npcs with the spawn.sql

---
+++ data/sql/optional/Spawn.sql
@@ -0,0 +1,35 @@
+-- Ian Drake
+DELETE FROM `creature` WHERE `id1` = 92001;
+DELETE FROM `creature` WHERE `id1` = 29093;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (92001, 0, 0, 0, 1, 1, -8807.04, 638.359, 94.312, 3.56047, 180, 0, 0, 'CodeExchange');
+
+-- Ransin Donner
+DELETE FROM `creature` WHERE `id1` = 92002;
+DELETE FROM `creature` WHERE `id1` = 2943;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (92002, 0, 0, 0, 1, 1, -4636.22, -1110, 501.411, 0.628319, 300, 0, 0, 'CodeExchange');
+
+-- Edward Carin
+DELETE FROM `creature` WHERE `id1` = 92003;
+DELETE FROM `creature` WHERE `id1` = 29095;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (92003, 0, 0, 0, 1, 1, 1581.83, 196.686, -43.1022, 2.25663, 180, 0, 0, 'CodeExchange');
+
+-- Zas'Tysh
+DELETE FROM `creature` WHERE `id1` = 92004;
+DELETE FROM `creature` WHERE `id1` = 7951;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (92004, 1, 0, 0, 1, 1, 1991.32, -4652.35, 27.3799, 0.10472, 300, 0, 0, 'CodeExchange');
+
+-- Tharl Stonebleeder
+DELETE FROM `creature` WHERE `id1` = 92005;
+DELETE FROM `creature` WHERE `id1` = 16076;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (92005, 1, 0, 0, 1, 1, 1989.31, -4655.91, 27.7722, 0.15708, 300, 0, 0, 'CodeExchange');
+
+-- Lando Longshot
+DELETE FROM `creature` WHERE `id1` = 92006;
+DELETE FROM `creature` WHERE `id1` = 17249;
+INSERT INTO `creature` (`id1`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `MovementType`, `Comment`)
+VALUES (92006, 0, 0, 0, 1, 1, -14397.5, 426.08, 7.97355, 5.51524, 300, 0, 0, 'CodeExchange');
